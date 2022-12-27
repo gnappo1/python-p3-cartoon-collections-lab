f0de83476328c51e9e91f980b1ef2c76bbed096f
Matteo Piccini <gnappo.1@live.it>
2022-12-27 14:48:15 -0800

    Done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,12 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    [print(f"{index + 1}. {dwarf}") for index, dwarf in enumerate(dwarves)]
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteers):
+    return [f"{planeteer.capitalize()}!" for planeteer in planeteers]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
 
-def find_the_cheese():
-    pass
+def find_the_cheese(words):
+    sol = [word for word in words if word in ["cheddar", "gouda", "camembert"]]
+    return sol[0] if len(sol) > 0 else None
